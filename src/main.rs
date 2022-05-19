aac779991163628d12c0f506b20a2136e5769ee7
IInfo <info@i1nfo.com>
2022-05-20 00:27:07 +0800

    Dev: Define CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::{env, error::Error, process};
+use clap::Parser;
+use std::error::Error;
+
+/// Arguments
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// Custom config file
+    #[clap(short, long)]
+    config: Option<String>,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("invalid count of arguments");
-        process::exit(1);
-    }
-    println!("{}", yxy::encrypt_password(&args[1], &args[2])?);
+    let args = Args::parse();
+    println!("{:?}", args);
 
     Ok(())
 }
